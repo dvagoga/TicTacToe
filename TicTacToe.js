5823273fede45a553215bfb6f58ac748c3d9111a
dvagoga <dvagoga@gmail.com>
2013-11-06 19:29:45 +0400

    add reset field

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,11 +1,19 @@
 // logic core level
+//
 // class with one instance
-function tGame(gField, width, numberOfPlayers){
-   this.gField = gField;
-   widthField = width;
-   nPl = numberOfPlayers - 1;
-   curPl = 0;
-//##--
+function tGame(width, numberOfPlayers){
+   var widthField = width;
+   var nPl = numberOfPlayers - 1;
+   var gField = [];
+   var curPl = 0;
+   var initChar = "n";
+//init field
+   this.initField = function(){
+      for (var i=0; i<widthField*widthField; i++){
+         gField[i] = initChar;
+      }
+   }
+//switch player
    this.nextPlayer = function(){
       if (curPl == nPl){
          curPl = 0;
@@ -15,111 +23,100 @@ function tGame(gField, width, numberOfPlayers){
       }
       return curPl;
    }
-//##--
+//check win
    this.checkWinner = function(){
-      var debugArr = [];
       var res = false;
       var curStr = 1;
+      var checkCell = function(ind1, ind2, ind3){
+         return gField[ind1] == gField[ind2] && gField[ind2] == gField[ind3] && gField[ind2] != initChar
+      };
       // check first line
       for(var i = 1; i < widthField-1; i++){
-         if (gField[i-1] == gField[i] && gField[i] == gField[i+1] && gField[i] != "n"){
-            res = true;
-         };
+            res = checkCell(i-1, i, i+1);
       };
       while(!res && curStr < (widthField-1)){
          // check first column
-         if (gField[widthField*curStr-widthField] == gField[widthField*curStr]
-            && gField[widthField*curStr] == gField[widthField*curStr+widthField]
-            && gField[widthField*curStr] != "n"){
-            res = true;
+         if (checkCell(widthField*curStr-widthField, widthField*curStr, widthField*curStr+widthField)){
+         res = true;
          };
          for(var i = widthField*curStr+1; i < widthField*(widthField-1); i++){
             // check middle lines
-            if (gField[i-1] == gField[i] && gField[i] == gField[i+1] && gField[i] != "n"){
-               res = true;
-            }
+            if (checkCell(i-1, i, i+1)) {res = true};
             // check middle column
-            if (gField[i-widthField] == gField[i] && gField[i] == gField[i+widthField] && gField[i] != "n"){
-               res = true;
-            }
+            if (checkCell(i-widthField, i, i+widthField)){res = true};
             // check fist diagonal
-            if (gField[i-widthField-1] == gField[i] && gField[i] == gField[i+widthField+1] && gField[i] != "n"){
-               res = true;
-            }
+            if (checkCell(i-widthField-1, i, i+widthField+1)){res = true};
             // check last diagonal
-            if (gField[i-widthField+1] == gField[i] && gField[i] == gField[i+widthField-1] && gField[i] != "n"){
-               res = true;
-            }
-         }
-         // check last column
-         if (gField[widthField*curStr-1] == gField[widthField*curStr+widthField-1]
-            && gField[widthField*curStr+widthField-1] == gField[widthField*curStr+widthField*2-1]
-            && gField[widthField*curStr+widthField-1] != "n"){
-            res = true;
+            if (checkCell(i-widthField+1, i, i+widthField-1)){res = true};
          };
+         // check last column
+         if (checkCell(widthField*curStr-1, widthField*curStr+widthField-1, widthField*curStr+widthField*2-1)){res = true};
          // next line
          curStr++;
       };
       // check last line
       for(var i = widthField*(widthField-1)+1; i < widthField*widthField; i++){
-         if (gField[i-1] == gField[i] && gField[i] == gField[i+1] && gField[i] != "n"){
-            res = true;
-         };
+         if (checkCell(i-1, i, i+1)){res = true};
       };
       return res;
    }
-//##--
+//set current cell
    this.setCell = function(index){
       var res = false;
-      if (gField[index] == "n"){
+      if (gField[index] == initChar){
          gField[index] = gField[index] + curPl;
          res = true;
       };
       return res;
    }
 };
-
-function initField(n, l){
-   var f = [];
-   for (var i=0; i<l*l; i++){
-      f.push("n");
-   }
-   var res = new tGame(f, l, n);
-   return res;
-};
-
-
-
-var numOfPlayers = 2;
-var numOfCells = 3;
-var player = false;
-
-var curGame = initField(numOfPlayers, numOfCells);
 // grafic user interface level
 // init
-var gField = document.getElementById('gameField');         
-for (var i=0; i<numOfCells; i++){
-   for(var j=0; j<numOfCells; j++){
-      var cell = document.createElement('div');
-      cell.className = 'celNul';
-      cell.setAttribute("celNum", i*numOfCells+j);
-      cell.addEventListener("click", getMove, false);
-      gField.appendChild(cell);
+function initGameField(numOfCells){
+   var pole = document.getElementById('gameField');         
+   var gField = document.createElement('div');
+   pole.appendChild(gField);
+   for (var i=0; i<numOfCells; i++){
+      for(var j=0; j<numOfCells; j++){
+         var cell = document.createElement('div');
+         cell.className = 'celNul';
+         cell.setAttribute("celNum", i*numOfCells+j);
+         cell.addEventListener("click", getMove, false);
+         gField.appendChild(cell);
+      };
+      var wall = document.createElement('div');
+      wall.className = 'wall';
+      gField.appendChild(wall);
    };
-   var wall = document.createElement('div');
-   wall.className = 'wall';
-   gField.appendChild(wall);
+};
+function resetGameField(){
+   var pole = document.getElementById('gameField');  
+   pole.removeChild(pole.getElementsByTagName('*')[0]);
 };
 // flow
-function getMove(){
-   
+function getMove(){   
    if (curGame.setCell(this.getAttribute('celNum'))){
-//alert(curGame.gField[this.getAttribute('celNum')]);
       this.className = "cel" + curGame.nextPlayer();
       if (curGame.checkWinner()){
-         alert("you win");
+         var plr = "nobody";
+         if (this.className == "cel0"){plr = "X";};
+         if (this.className == "cel1"){plr = "O";};
+         if (this.className == "cel2"){plr = "Z";};
+         alert(plr + ", you win");
+         curGame.initField();
+         resetGameField();
+         initGameField(numOfCells);
       };
    };
-   //alert(curGame.widthField);
 };
+function makeGame(){
+   var q = document.getElementsByTagName('input');
+   alert(q[0].name);
+};
+// create obj
+var numOfPlayers = 3;
+var numOfCells = 10;
 
+var curGame = new tGame(numOfCells, numOfPlayers);
+curGame.initField();
+initGameField(numOfCells);
